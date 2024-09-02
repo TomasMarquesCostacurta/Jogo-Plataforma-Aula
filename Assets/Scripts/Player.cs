63c70bb17c99f019be1ef6c7be651617e63bf167
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 10:09:05 -0300

    bomba

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float horizontal;
+    private Rigidbody2D rb;
+    private bool isFacingRight = true;
+    // Update is called once per frame
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        this.rb.velocity = new Vector2(horizontal * 8f, rb.velocity.y);
+        Flip();
     }
-}
+    private void Flip() { 
+    if(isFacingRight && horizontal < 0f || isFacingRight && horizontal>0){
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale; 
+        }
+    
+}}
