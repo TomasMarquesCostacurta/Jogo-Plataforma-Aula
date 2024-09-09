e248ba2fd42607dcbcec72ba87fcfcdbc258fab8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-09 10:11:30 -0300

    Cold

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float speed = 8f;
     private float horizontal;
     private Rigidbody2D rb;
+    private Animator animator;
     private bool isFacingRight = true;
     // Update is called once per frame
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         Debug.Log(horizontal);
-        this.rb.velocity = new Vector2(horizontal * 8f, rb.velocity.y);
+        this.rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+             this.rb .AddForce(Vector2.up *8F, ForceMode2D.Impulse);
+        }
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
         Flip();
     }
     private void Flip() { 
-    if(isFacingRight && horizontal < 0f || isFacingRight && horizontal>0){
+    if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal>0){
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
